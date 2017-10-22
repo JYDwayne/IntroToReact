d0d4f1c0edfa7dfc2ebd5af990ccefd2911fa5d7
Duan,Junyan <duanjunyan@bjzjns.com>
2017-10-22 23:44:45 +0800

    "add code"

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -33,7 +33,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = this.state.xIsNext ? 'Next player: X' : 'Next player: O';
+    const winner = calculateWinner(this.state.arr);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -58,6 +64,9 @@ class Board extends React.Component {
 
   changeData(data) {
     const arr = this.state.arr.slice();
+    if (calculateWinner(arr) || arr[data]) {
+      return
+    }
     arr[data] = this.state.xIsNext? 'x':'o';
     this.setState({arr: arr, xIsNext: !this.state.xIsNext})
   }
@@ -86,3 +95,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
